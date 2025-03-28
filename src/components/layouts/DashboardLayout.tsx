e8f460fabdbd4210e0cdda5b81a4a7d2bf410a04
Babyze <danghuynhanh1999@gmail.com>
2025-03-28 16:16:33 +0700

    Commit Sidebar UI

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router';
+import Sidebar from './Sidebar/Sidebar';
 import './DashboardLayout.css';
 
 const DashboardLayout = () => {
   return (
-    <div className="dashboard-layout">
-      {/* <Sidebar className="dashboard-layout__sidebar" /> */}
-      <div className="dashboard-layout__main">
-        {/* <Header className="dashboard-layout__header" /> */}
-        <main className="dashboard-layout__content">
+    <div className="dashboard">
+      <Sidebar />
+      <main className="dashboard__main">
+        <div className="dashboard__content">
           <Outlet />
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 };
